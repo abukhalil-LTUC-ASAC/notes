12b22fe8fdfbe47a7c801025b77d0303b1c7e4d9
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-09-09 22:18:34 +0300

    hotfix

--- notes.js
+++ notes.js
@@ -2,19 +2,20 @@
 
 'use strict';
 
-const Input = require('./lib/input.js');
-const Notes = require('./lib/notes.js');
 const mongoose = require('mongoose');
-
-const input = new Input();
-const notes = new Notes(input);
 const MONGOOSE_URL = 'mongodb://localhost:27017/notes';
-
 mongoose.connect(MONGOOSE_URL, {
   useNewUrlParser : true,
   useUnifiedTopology : true,
   useCreateIndex : true,
   useFindAndModify : false
 });
+
+
+const Input = require('./lib/input.js');
+const Notes = require('./lib/notes.js');
+
+const input = new Input();
+const notes = new Notes(input);
 
 notes.valid() ? notes.execute() : notes.help();
