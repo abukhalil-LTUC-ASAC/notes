fe54482cefad1f246d976c8351294e2e90ac8e77
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-09-12 04:26:30 +0300

    hotfix

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -7,7 +7,7 @@ require('@code-fellows/supergoose');
 
 describe('Note Model', () => {
   it('can create a new Note item', async () => {
-      let obj = { Text: 'test Note 1', Category: 'Schedule'};
+      let obj = { text: 'test Note 1', category: 'Schedule'};
       const record = await NotesDB.save(obj);
       Object.keys(obj).forEach(key => {
         expect(record[key]).toEqual(obj[key]);
@@ -15,7 +15,7 @@ describe('Note Model', () => {
   });
 
   it('can get() a note item', async ()=> {
-    let obj = { Text: 'test Note 2', Category: 'Study'};
+    let obj = { text: 'test Note 2', category: 'Study'};
     const record = await NotesDB.save(obj);
     const note = await NotesDB.get(record._id);
     Object.keys(obj).forEach(key => {
@@ -24,7 +24,7 @@ describe('Note Model', () => {
   }); 
 
   it('can delete() a note item', async ()=> {
-    let obj = { Text: 'test Note 3', Category: 'Work'};
+    let obj = { text: 'test Note 3', category: 'Work'};
     const record = await NotesDB.save(obj);
     await NotesDB.deleteOne(record._id);
     const note = await NotesDB.get(record._id);
@@ -34,7 +34,7 @@ describe('Note Model', () => {
   }); 
 
   it('can update() a note item', async ()=> {
-    let obj = { Text: 'test Note 4', Category: 'Morning'};
+    let obj = { text: 'test Note 4', category: 'Morning'};
     const record = await NotesDB.save(obj);
 
     let search = {updateID: `${record._id}`, update: {Text: 'Not a note anymore', Category: 'Evening'}}; // tests with custom input
